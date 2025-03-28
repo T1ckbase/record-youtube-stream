c5c059a5a22ea1133c7ef6f2b043d4063df4c829
T1ckbase <146760065+T1ckbase@users.noreply.github.com>
2025-03-28 17:10:56 +0800

    update path

--- yt-dlp.ts
+++ yt-dlp.ts
@@ -1,18 +1,18 @@
 import { exists } from '@std/fs';
 
 export async function ensureYtDlp() {
-  if (await exists('yt-dlp', { isFile: true })) return;
+  if (await exists('./yt-dlp', { isFile: true })) return;
   console.info('Downloading yt-dlp...');
   const res = await fetch(`https://github.com/yt-dlp/yt-dlp/releases/latest/download/yt-dlp${Deno.build.os === 'windows' ? '.exe' : ''}`);
   if (!res.ok) throw Error(`Failed to download yt-dlp: ${res.status} ${res.statusText}`);
   const data = await res.arrayBuffer();
-  await Deno.writeFile('yt-dlp', new Uint8Array(data));
+  await Deno.writeFile('./yt-dlp', new Uint8Array(data));
 }
 
 export async function downloadVideo(videoId: string, path?: string) {
   const args = ['--live-from-start', videoId, '-f', 'bv+ba/b'];
   path && args.push('--paths', path);
-  const command = new Deno.Command('yt-dlp', {
+  const command = new Deno.Command('./yt-dlp', {
     args,
     stdin: 'piped',
     stdout: 'piped',
